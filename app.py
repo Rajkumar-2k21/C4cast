2cfdb8567974f9e3583ff1145120732d6c9f7a91
Rajkumar-2k21 <rajempire2k20@gmail.com>
2022-12-04 13:16:00 +0530

    commit

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import requests
 
-api_key=
+api_key='f8db2868af87e9e4244dcbd14f131c24'
+
+user_input=input("enter the city name:")
+
+weather_data=requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}")
+
+
+weather=weather_data.json()['weather'][0]['main']
+temp=weather_data.json()['main']['temp']
+hum=weather_data.json()['main']['humidity']
+print("current weather is :",weather)
+print("current temprature is :",temp)
+print("current humadity is :",hum) 
+
