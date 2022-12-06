6e8f6edd86bd4d6cf1fbf0b60ca5e1f265e48a87
Rajkumar-2k21 <rajempire2k20@gmail.com>
2022-12-06 15:40:37 +0530

    commit

--- app.py
+++ app.py
@@ -4,25 +4,37 @@ app=Flask(__name__,template_folder='template',static_url_path='/static')
 
 api_key='64b497fbf20a8bd6a9b6758e738bc6a1'
 
+
 @app.route('/', methods=["GET","POST"])
 def weather():
-   return render_template('index.html')
+    
+    return render_template('index.html')
 
 @app.route("/weather", methods =["GET","POST"])
 def home():
     if request.method=='POST':
-        cityname =request.form['cityname']
-        url='https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='+api_key
-        res=requests.get(url.format(cityname)).json()
-        
-        temp=res['main']['temp']
-        hmt=res['main']['humidity']
-        weather=res['weather'][0]['description']
-        sys=res['sys']['country']
-        min=res['main']['temp_min']
-        max=res['main']['temp_max']
+        cityname =request.form["cityname"]
+        url='https://api.openweathermap.org/data/2.5/weather?q='+cityname+'&units=metric&appid='+api_key
+        link=requests.get(url)
+        res=link.json()
+        if res['cod']=='404':
+            return render_template('404.html')
+        else:
+
+            temp=res['main']['temp']
+            hmt=res['main']['humidity']
+            weather=res['weather'][0]['description']
+            sys=res['sys']['country']
+            min=res['main']['temp_min']
+            max=res['main']['temp_max']
         
         return render_template('weather.html',temp=temp,hmt=hmt,cityname=cityname,weather=weather,sys=sys,min=min,max=max)
+@app.route('/login',methods=["GET","POST"])
+def login():
+        return render_template('login.html')
+@app.route('/register',methods=["GET","POST"])
+def login():
+        return render_template('register.html')
 
 if __name__=='__main__':
     app.run(debug=True)
