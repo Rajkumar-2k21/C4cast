4fe41acb0864cd7f4d373d46a26db029a762b8ff
Rajkumar-2k21 <rajempire2k20@gmail.com>
2022-12-09 15:10:09 +0530

    commit

--- app.py
+++ app.py
@@ -1,14 +1,45 @@
-from flask import Flask,render_template,request
+import pyrebase
 import requests
+from flask import Flask,render_template,request
+
+
+config = {
+     'apiKey': "AIzaSyAWSn6bz7FnBiLI2rfDsC007YRWuF2lqEw",
+    'authDomain': "auth-753a0.firebaseapp.com",
+    'projectId': "auth-753a0",
+    'storageBucket': "auth-753a0.appspot.com",
+    'messagingSenderId': "586710427740",
+    'appId': "1:586710427740:web:0a9ec2988eec3c8e071e99",
+    'measurementId': "G-D4HHWJ9MLB",
+   "databaseURL":"https://auth-753a0-default-rtdb.firebaseio.com/"
+}
+firebase = pyrebase.initialize_app(config)
+auth=firebase.auth()
+db = firebase.database()
+
+
 app=Flask(__name__,template_folder='template',static_url_path='/static')
 
 api_key='64b497fbf20a8bd6a9b6758e738bc6a1'
 
+@app.route('/login',methods=["GET","POST"])
+def login():
+        return render_template('login.html')
 
-@app.route('/', methods=["GET","POST"])
+
+@app.route('/index', methods=["GET","POST"])
 def weather():
     
     return render_template('index.html')
+
+@app.route('/')
+def first():
+    
+    return render_template('first.html')
+
+@app.route('/register',methods=["GET","POST"])
+def register():
+        return render_template('register.html')    
 
 @app.route("/weather", methods =["GET","POST"])
 def home():
@@ -29,12 +60,7 @@ def home():
             max=res['main']['temp_max']
         
         return render_template('weather.html',temp=temp,hmt=hmt,cityname=cityname,weather=weather,sys=sys,min=min,max=max)
-@app.route('/login',methods=["GET","POST"])
-def login():
-        return render_template('login.html')
-@app.route('/register',methods=["GET","POST"])
-def login():
-        return render_template('register.html')
+
 
 if __name__=='__main__':
     app.run(debug=True)
