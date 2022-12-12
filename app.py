50cf1b36f3ade1c874b5c97fe6618be2c3d114af
Rajkumar-2k21 <rajempire2k20@gmail.com>
2022-12-13 00:05:10 +0530

    done

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pyrebase
 import requests
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for,flash,session, abort
 
 
 config = {
@@ -11,7 +11,7 @@ config = {
     'messagingSenderId': "586710427740",
     'appId': "1:586710427740:web:0a9ec2988eec3c8e071e99",
     'measurementId': "G-D4HHWJ9MLB",
-   "databaseURL":"https://auth-753a0-default-rtdb.firebaseio.com/"
+   'databaseURL':"https://auth-753a0-default-rtdb.firebaseio.com/"
 }
 firebase = pyrebase.initialize_app(config)
 auth=firebase.auth()
@@ -22,24 +22,76 @@ app=Flask(__name__,template_folder='template',static_url_path='/static')
 
 api_key='64b497fbf20a8bd6a9b6758e738bc6a1'
 
-@app.route('/login',methods=["GET","POST"])
-def login():
-        return render_template('login.html')
+person = {"is_logged_in": False, "name": "", "email": "", "uid": ""}
 
 
 @app.route('/index', methods=["GET","POST"])
-def weather():
-    
-    return render_template('index.html')
-
+def index():
+    if person["is_logged_in"] == True:
+        return render_template("index.html",name = person["name"])
+    else:
+        return redirect('login.html')
 @app.route('/')
 def first():
-    
-    return render_template('first.html')
+        return render_template('first.html')
 
+@app.route('/login')
+def login():
+        return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+        return render_template('signup.html')
+
+@app.route("/result", methods = ["POST", "GET"])
+def result():
+    if request.method == "POST":       
+        result = request.form          
+        email = result["email"]
+        password = result["pass"]
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            global person
+            person["is_logged_in"] = True
+            person["email"] = user["email"]
+            person["uid"] = user["localId"]
+            data = db.child("users").get()
+            person["name"] = data.val()[person["uid"]]["name"]
+            return redirect(url_for('index'))
+        except:
+            return redirect(url_for('login'))
+    else:
+        if person["is_logged_in"] == True:
+            return redirect(url_for('index'))
+        else:
+             flash('Mail id Password wrong')  
+             return redirect(url_for('login'))
+             
 @app.route('/register',methods=["GET","POST"])
 def register():
-        return render_template('register.html')    
+    if request.method == "POST":        
+        result = request.form           
+        email = result["email"]
+        password = result["pass"]
+        name = result["name"]
+        try:
+            auth.create_user_with_email_and_password(email, password)
+            user = auth.sign_in_with_email_and_password(email, password)
+            global person
+            person["is_logged_in"] = True
+            person["email"] = user["email"]
+            person["uid"] = user["localId"]
+            person["name"] = name
+            data = {"name": name, "email": email}
+            db.child("users").child(person["uid"]).set(data)
+            return redirect(url_for('index'))
+        except:
+            return redirect(url_for('signup'))
+    else:
+        if person["is_logged_in"] == True:
+            return redirect(url_for('index'))
+        else:
+            return redirect(url_for('signup'))
 
 @app.route("/weather", methods =["GET","POST"])
 def home():
